12110d01287ee342e5325a91389699e544d30087
marques vendas <marquesfilhoantonio777@gmail.com>
2026-01-24 21:18:56 -0300

    Update mp.js

--- pages/mp.js
+++ pages/mp.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import Head from 'next/head';
-import Cart from './Cart';
+import Cart from '../components/Cart';
 import Link from 'next/link';
 
 // ========== CONFIGURAÇÃO LOCAL ========== //
