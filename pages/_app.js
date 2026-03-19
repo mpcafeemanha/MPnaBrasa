fe7b64739fb83b5929677ad9f2a9694a46b4dab8
mpcafeemanha <mpcafeemanha@gmail.com>
2026-03-19 09:41:41 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,26 @@
 // pages/_app.js - VERSÃO SIMPLES SEM CSS
+import Script from 'next/script';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-L9QFXDT8PL"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-L9QFXDT8PL');
+        `}
+      </Script>
+      
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
